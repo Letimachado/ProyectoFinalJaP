69d14299bc45511ef51406db87834cc0beec2005
Magalí Díaz <yajaira281096@gmail.com>
2024-08-29 21:22:49 -0300

    corregimos la ubicación del desafiate

--- js/init.js
+++ js/init.js
@@ -39,3 +39,26 @@ let getJSONData = function(url){
         return result;
     });
 }
+
+ // Verificación de sesión de usuario
+ const loginExitoso = localStorage.getItem('UsuarioRegistrado');
+
+
+ if (!loginExitoso) {
+     window.location.href = 'login.html'; // Redirige a la página de inicio de sesión si no hay sesión
+ } else {
+     console.log("El usuario ya está registrado");
+
+
+     // Mostrar el correo del usuario en el header
+     const userEmailElement = document.getElementById('userEmail'); // Obtén el elemento donde se mostrará el correo
+     const email = localStorage.getItem('email'); // Recupera el correo del usuario de localStorage
+    
+     if (userEmailElement) {
+         if (email) {
+             userEmailElement.textContent = email; // Muestra el correo si está disponible
+         } else {
+             userEmailElement.textContent = 'Usuario'; // Muestra un texto por defecto si no hay correo
+         }
+     }
+ }
