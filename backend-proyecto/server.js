13aea3c201153d588d036c39396e649e82940004
Letimachado <leti.machado.gerez@gmail.com>
2024-11-29 10:41:38 -0300

    Punto 3 entrega 8 finfin

--- backend-proyecto/server.js
+++ backend-proyecto/server.js
@@ -6,12 +6,8 @@ const jwt = require('jsonwebtoken');
 const SECRET_KEY = "accesoprivado"; 
 
 
-
-
-// Middleware
 app.use(cors());
 
-// Rutas para los JSONs
 app.get('/categories', (req, res) => {
     res.sendFile(__dirname + '/data/cats/cat.json');
 });
@@ -36,36 +32,35 @@ app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 });
 
-// Middleware para procesar JSON en el cuerpo de las peticiones
 app.use(express.json());
 
-// Ruta POST para /login
+// Endpoint POST
 app.post('/login', (req, res) => {
-    const { username, password } = req.body; // Extraer usuario y contraseña del cuerpo
+    const { username, password } = req.body; 
 
-    // Datos de ejemplo (puedes reemplazar esto con un sistema de base de datos)
     const usuarios = [
-        { username: 'admin', password: '1234' },
-        { username: 'usuario1', password: '5678' },
+        { username: 'adminLeti', password: '1234' },
+        { username: 'adminLu', password: '5678' },
+        { username: 'adminRo', password: '1011' },
+        { username: 'adminMaga', password: '1213' },
     ];
 
-    // Verificar si el usuario existe y la contraseña es correcta
     const usuarioEncontrado = usuarios.find(
         user => user.username === username && user.password === password
     );
 
     if (usuarioEncontrado) {
-        // Generar el token
+        // TOKEN
         const token = jwt.sign(
-            { username: usuarioEncontrado.username }, // Payload del token
-            SECRET_KEY,                               // Clave secreta
-            { expiresIn: '1h' }                       // Opciones (token expira en 1 hora)
+            { username: usuarioEncontrado.username }, 
+            SECRET_KEY,                               
+            { expiresIn: '1h' }                       // Expira en 1 hora
         );
 
-        // Responder con el token
+
         res.json({ status: 'ok', token });
     } else {
-        // Usuario o contraseña incorrectos
+   
         res.status(401).json({ status: 'error', message: 'Credenciales inválidas' });
     }
 });
